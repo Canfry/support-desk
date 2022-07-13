2be293da3ec73bb9e560c8c0be0cfbe977055de0
fry <fry.land@hotmail.fr>
2022-07-13 22:51:01 +0200

    Error & Exception Handling

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./midlleware/errorMiddleware');
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+// Middleware to use req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to  the Support Desk API' });
@@ -11,5 +16,8 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
+
+// Middleware
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
