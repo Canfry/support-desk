b2727daab23c576e39019f0e9482f0526b1803a0
fry <fry.land@hotmail.fr>
2022-07-15 10:10:36 +0200

    db setup and connection and register user

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./midlleware/errorMiddleware');
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+// COnnect to DB
+connectDB();
 
 // Middleware to use req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to  the Support Desk API' });
+  res.status(200).json({ message: 'Welcome to the Support Desk API' });
 });
 
 // Routes
