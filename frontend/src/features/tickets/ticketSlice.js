3673e1f7a489602eca5357b995e87bd33bd1790d
fry <fry.land@hotmail.fr>
2022-07-27 22:52:36 +0200

    Add tickets to Redux + tickets functionality

---
+++ frontend/src/features/tickets/ticketSlice.js
@@ -0,0 +1,61 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import ticketService from './ticketService';
+
+const initialState = {
+  tickets: [],
+  ticket: {},
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+};
+
+// CREATE Ticket
+export const createTicket = createAsyncThunk(
+  'tickets/create',
+  async (ticketData, thunkAPI) => {
+    try {
+      // Get token as it's protected route
+      const token = thunkAPI.getState().auth.user.token;
+      return await ticketService.createTicket(ticketData, token);
+    } catch (error) {
+      // Put the error message in our message auth InitialState
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const ticketSlice = createSlice({
+  name: 'ticket',
+  initialState,
+  reducers: {
+    // Reset default once fulfilled
+    reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createTicket.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createTicket.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.ticket = action.payload;
+      })
+      .addCase(createTicket.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
+});
+
+export const { reset } = ticketSlice.actions;
+export default ticketSlice.reducer;
