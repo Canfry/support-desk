4946d37846a89402887fce651d55383f7620e875
fry <fry.land@hotmail.fr>
2022-08-30 07:15:48 +0200

    fetch notes through Redux

---
+++ frontend/src/features/notes/noteService.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_URL = 'api/tickets/';
+
+// Get Ticket Notes
+const getNotes = async (ticketData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.post(API_URL + ticketData + '/notes', config);
+
+  return response.data;
+};
+
+const noteService = { getNotes };
+
+export default noteService;
---
+++ frontend/src/features/notes/noteSlice.js
@@ -0,0 +1,60 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import noteService from './noteService';
+
+const initialState = {
+  notes: [],
+  isError: false,
+  isLoading: false,
+  isSuccess: false,
+  message: '',
+};
+
+// Get ticket Notes
+export const getNotes = createAsyncThunk(
+  'notes/getAll',
+  async (ticketId, thunkAPI) => {
+    try {
+      // Get token as it's protected route
+      const token = thunkAPI.getState().auth.user.token;
+      return await noteService.getNotes(ticketId, token);
+    } catch (error) {
+      // Put the error message in our message auth InitialState
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// Create Slice
+export const noteSlice = createSlice({
+  name: 'note',
+  initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getNotes.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getNotes.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.notes = action.payload;
+      })
+      .addCase(getNotes.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
+});
+
+export const { reset } = noteSlice.actions;
+export default noteSlice.reducer;
